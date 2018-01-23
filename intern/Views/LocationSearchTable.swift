7b2a8f2bbdb135cf5d0d9455ae7bf31a3482f3c3
Jian Zhai <zhaijianjian@gmail.com>
2018-01-23 11:53:14 -0800

    add address to detailTextLabel

--- intern/Views/LocationSearchTable.swift
+++ intern/Views/LocationSearchTable.swift
@@ -43,7 +43,31 @@ extension LocationSearchTable {
         let cell = tableView.dequeueReusableCell(withIdentifier: "searchBarCell")!
         let selectedItem = matchingItems[indexPath.row].placemark
         cell.textLabel?.text = selectedItem.name
-        cell.detailTextLabel?.text = ""
+        cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
         return cell
     }
+}
+
+func parseAddress(selectedItem:MKPlacemark) -> String {
+    // put a space between number and name
+    let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+    // put a comma between street and city/state
+    let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+    // put a space between city and state
+    let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+    let addressLine = String(
+        format:"%@%@%@%@%@%@%@",
+        // street number
+        selectedItem.subThoroughfare ?? "",
+        firstSpace,
+        // street name
+        selectedItem.thoroughfare ?? "",
+        comma,
+        // city
+        selectedItem.locality ?? "",
+        secondSpace,
+        // state
+        selectedItem.administrativeArea ?? ""
+    )
+    return addressLine
 }
