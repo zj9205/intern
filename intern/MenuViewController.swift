7bba6c94e732f660e52806dc4b4340eda25d85cd
Jian Zhai <zhaijianjian@gmail.com>
2018-01-22 23:11:36 -0800

    add button for dismiss the menu view

---
+++ intern/MenuViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MenuViewController.swift
+//  intern
+//
+//  Created by Jian Zhai on 2018/1/22.
+//  Copyright © 2018年 Jian Zhai. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
